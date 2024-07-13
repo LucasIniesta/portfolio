65697e787610d5fd7ba19e9968d199fa73cd1970
LucasIniesta <l.iniesta.94@gmail.com>
2024-07-13 11:22:08 -0300

    Adição de interatividade nos cards de projetos

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,16 +1,31 @@
 import styles from './Card.module.css'
 import ButtonA from './ButtonA'
+import {useState} from 'react'
 
 function Card({image, title, tech, description, repo, site}){
+    const [showData, setShowData] = useState(false)
+
+    function dataOn(){
+        setShowData(true)
+    }
+
+    function dataOf(){
+        setShowData(false)
+    }
+    
     return(
-        <div className={styles.card}>
-            <a href={site} target='_blank'><img src={image}/></a> 
-            <section>
-                <h3>{title}</h3>
-                <p><strong>Tecnologias: </strong> {tech}</p>
-                <p>{description}</p>
-                <ButtonA link={repo} text='Acesse o repositório'/>
-            </section>
+        <div onMouseLeave={dataOf} className={styles.card}>
+            <a onMouseEnter={dataOn} href={site} target='_blank'><img src={image}/></a> 
+
+            {showData &&(
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologias: </strong> {tech}</p>
+                    <p>{description}</p>
+                    <ButtonA link={repo} text='Acesse o repositório'/>
+                </section>
+                )}
+
         </div>
     )
 }
